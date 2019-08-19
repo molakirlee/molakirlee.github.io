b2885578682f2694808e147f46328bebd61d88aa
molakirlee <molakirlee@sina.com>
2019-08-19 09:51:30 +0800

    update

---
+++ _posts/《斤竹精舍·游艺集》/GMX/2019-08-18-MD_knowledge.markdown
@@ -0,0 +1,30 @@
+---
+layout:     post
+title:      "MD知识"
+subtitle:   ""
+date:       2019-04-25 20:38:00
+author:     "XiLock"
+header-img: ""
+header-mask: 0.3
+catalog:    true
+tags:
+    - 《斤竹精舍·游艺集》
+    - GMX学习记录
+    - 2019
+
+---
+
+
+### 常识
+###### 离子的水合半径
+离子的水合半径与离子的电荷密度、离子势有关，从2倍到14倍不等。参考[常见离子的水合半径](http://railsback.org/Fundamentals/815HydratedCationRadii04LS.pdf)  
+
+
+### 杂记
+1. 纤维素可用glycam力场做（参见李老师博客）；
+1. gaff描述有机小分子可以，高分子的话用opls更好；
+1. ambertool可以做分子拓扑的拼接，比如分别做PET中间和两端的三种非标准残基，然后将其拼接起来（参见amber的官网教程）；
+
+
+
+![](/img/wc-tail.GIF)
--- attachment/gmx/Matlab_cal_salt_bridge.m
+++ attachment/gmx/Matlab_cal_salt_bridge.m
@@ -3,8 +3,14 @@ clear all
 %load file
 FN=dir(['*.dat']);
 %Creat a matrix with serial number
+%读取dat文件，取其横坐标写入C0
 C00=load(FN(1).name);
 C0=C00(:,1);
+%给不同条件下的C横坐标赋值
+Cs=C0;%相同链,same
+Cd=C0;%不同链,different
+Ct=C0;%所有链,total
+%----------------------------------------------------------
 %iteration
 %
 for n=1:length(FN) %迭代所有.dat文件
@@ -25,31 +31,71 @@ for n=1:length(FN) %迭代所有.dat文件
     A4(5)={char(A5(1))};
     A4(6)={char(A5(2))};
     A4;
-    %What to do if in the same chain
-    %Remove first column and combine data
-    if char(A4(3))==char(A4(5)) %若同一条链则进行如下处理
-        [rC0,cC0]=size(C0);
-        B=load(FN(n).name);
-        B(:,[1])=[];
-        C0=[C0,B];
-    end
-
+    %分别处理同链盐桥、异链盐桥、总盐桥
+     if char(A4(3))==char(A4(5)) %若同一条链则进行如下处理
+        B=load(FN(n).name);%读取第n个dat文件的内容
+        B(:,[1])=[];%删除第一列数据，及删除横坐标
+        Cs=[Cs,B];%将该组数据写入Cs之中,same
+        else
+        B=load(FN(n).name);%读取第n个dat文件的内容
+        B(:,[1])=[];%删除第一列数据，及删除横坐标
+        Cd=[Cd,B];%将该组数据写入Cd之中,different
+     end
+     B=load(FN(n).name);%读取第n个dat文件的内容
+     B(:,[1])=[];%删除第一列数据，及删除横坐标
+     Ct=[Ct,B];%将该组数据写入Cd之中,total
 end
-%Calculate salt bridge
+%----------------------------------------------------
+%Calculate salt bridge between same chain
 %
-[Rnum,Cnum]=size(C0);
+[Rnum,Cnum]=size(Cs);%读取Cs的行/列数
 k=0;
 for i=1:Rnum
     for j=2:Cnum
-        if C0(i,j)<4
+        if Cs(i,j)<4
             k=k+1;
         end
     end
-    C0(i,j+1)=k;
+    Cs(i,j+1)=k;
     k=0;
 end
+%Calculate salt bridge between different chain
+%
+[Rnum,Cnum]=size(Cd);%读取Cd的行/列数
+k=0;
+for i=1:Rnum
+    for j=2:Cnum
+        if Cd(i,j)<4
+            k=k+1;
+        end
+    end
+    Cd(i,j+1)=k;
+    k=0;
+end
+%Calculate salt bridge totally
+%
+[Rnum,Cnum]=size(Ct);%读取Cd的行/列数
+k=0;
+for i=1:Rnum
+    for j=2:Cnum
+        if Ct(i,j)<4
+            k=k+1;
+        end
+    end
+    Ct(i,j+1)=k;
+    k=0;
+end
+%----------------------------------------------------------
 %Write to xls
-xlswrite('Salt_bridge_same_chain.xls',C0)
+% xlswrite('Salt_bridge_chain_s.xls',Cs)
+% xlswrite('Salt_bridge_chain_d.xls',Cd)
+% xlswrite('Salt_bridge_chain_t.xls',Ct)%如果数量太多的话可能写出会报错
+%Write to txt
+CA=[Cs(:,end),Cd(:,end),Ct(:,end)];%统计盐桥情况，用于analyze
+xlswrite('Salt_bridge_chain_a.xls',CA)
+dlmwrite('Salt_bridge_chain_s.txt',Cs)
+dlmwrite('Salt_bridge_chain_d.txt',Cd)
+dlmwrite('Salt_bridge_chain_t.txt',Ct)
 %---------------------
 %---------------------
 %---------------------
