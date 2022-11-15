7a8621793bb5158994b2ac725f9ad4393ed5c0a3
unknown <molakirlee@sina.com>
2022-11-15 22:15:12 +0800

    update

--- quotes.js
+++ quotes.js
@@ -898,7 +898,7 @@ var quotes = [
 'The struggle makes everything all the more worth it.',
 'The most exhausting thing in life is being insincere.',
 'What we call "failure" is not falling down, but the staying down.',
-'To infinity and beyond! 飞向宇宙，浩瀚无垠！',
+'To infinity and beyond! 飞向宇宙，浩瀚无垠!',
 'Pain is inevitable. Suffering is optional.',
 'Miracles happen every day.',
 'Nothing for nothing.',
